f86b2061044717729312827a41af80faed6276bc
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2025-07-27 16:37:56 +0200

    integrated Prettier, ESLint, and Husky

--- client/app/all-apartments/page.tsx
+++ client/app/all-apartments/page.tsx
@@ -7,6 +7,8 @@ import { ContractsChart } from '@/components/Chart/Chart';
 const Page = () => {
   const unusedVar = 123;  // Цю змінну не використовуємо, має бути помилка
   const unusedVar1 = 123;  // Цю змінну не використовуємо, має бути помилка
+  const unusedVar1 = 123;  // Цю змінну не використовуємо, має бути помилка
+  const unusedVar1 = 123;  // Цю змінну не використовуємо, має бути помилка
   const usedVar = 456;
   console.log(usedVar);
 
