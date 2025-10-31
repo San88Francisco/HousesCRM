99a9c81e1ec16dadd6eaaf143b53b4880c1abbc9
Denis Tkachenko <113258288+Tkachenko01001@users.noreply.github.com>
2025-10-31 02:33:57 +0200

    [ADD]: house detail analytic service

--- server/src/houses/houses.module.ts
+++ server/src/houses/houses.module.ts
@@ -4,9 +4,9 @@ import { HousesController } from './houses.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { House } from './entities/house.entity'
 import { HousePriceModule } from 'src/house-prices/house-price.module'
-import { HousesAnalyticsModule } from 'src/analytics/houses-analytics/houses-analytics.module'
+import { AnalyticsModule } from 'src/analytics/analytics.module'
 @Module({
-  imports: [TypeOrmModule.forFeature([House]), HousePriceModule, HousesAnalyticsModule],
+  imports: [TypeOrmModule.forFeature([House]), HousePriceModule, AnalyticsModule],
   controllers: [HousesController],
   providers: [HousesService],
 })
