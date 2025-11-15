1831887e9be70e45fe6c72f21bc1ecd7c0bb4fda
Xlussov <130926999+Xlussov@users.noreply.github.com>
2025-11-15 22:05:28 +0200

    fixed error text field

--- client/middleware.ts
+++ client/middleware.ts
@@ -4,7 +4,7 @@ import { ROUTES } from '@/shared/routes';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const PUBLIC_ROUTES = [ROUTES.LOGIN];
+const PUBLIC_ROUTES = [ROUTES.ALL_APARTMENTS, ROUTES.HOME, ROUTES.UIKIT];
 
 const AUTH_ROUTES = [ROUTES.LOGIN];
 
--- client/src/shared/ui/input.tsx
+++ client/src/shared/ui/input.tsx
@@ -3,7 +3,6 @@
 import { CircleAlert } from 'lucide-react';
 import { ComponentProps, forwardRef, ReactNode, useState } from 'react';
 import { cn } from '../utils/cn';
-/* eslint-disable */
 
 interface InputProps extends ComponentProps<'input'> {
   error?: string;
@@ -16,39 +15,44 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
     const [isFocused, setIsFocused] = useState<boolean>(false);
 
     return (
-      <div
-        className={cn(
-          ' flex items-center h-10 px-2 w-full text-sm transition-all duration-200 ease-in-out bg-bg-input rounded-lg border border-solid border-border [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4',
-          disabled && 'cursor-not-allowed opacity-50',
-          isFocused && 'border-active-border',
-          error && 'border-red text-red',
-          className,
-        )}
-      >
-        {icon && (
-          <span
+      <div className={cn(className, "relative")}>
+        <div
+          className={cn(
+            'flex items-center h-10 px-2 w-full text-sm',
+            'transition-all duration-200 ease-in-out',
+            'bg-bg-input rounded-lg border border-solid border-border',
+            '[&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4',
+            disabled && 'cursor-not-allowed opacity-50',
+            isFocused && 'border-active-border',
+            error && 'border-red text-red',
+          )}
+        >
+          {icon && (
+            <span
+              className={cn(
+                'transition-all duration-200 ease-in-out text-muted',
+                isFocused && 'text-active-border',
+                error && 'text-red',
+              )}
+            >
+              {icon}
+            </span>
+          )}
+          <input
             className={cn(
-              'transition-all duration-200 ease-in-out text-muted',
-              isFocused && 'text-active-border',
+              'flex w-full px-2 bg-inherit text-text text-sm ring-offset-none placeholder:text-muted focus-visible:outline-none focus-visible:none disabled:cursor-not-allowed disabled:opacity-50',
               error && 'text-red',
             )}
-          >
-            {icon}
-          </span>
-        )}
-        <input
-          className={cn(
-            'flex w-full px-2 bg-inherit text-text text-sm ring-offset-none placeholder:text-muted focus-visible:outline-none focus-visible:none disabled:cursor-not-allowed disabled:opacity-50',
-            error && 'text-red',
-          )}
-          type={type}
-          ref={ref}
-          disabled={disabled}
-          {...props}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-        />
-        {error && iconWithError && <CircleAlert className="text-red" />}
+            type={type}
+            ref={ref}
+            disabled={disabled}
+            {...props}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+          />
+          {error && iconWithError && <CircleAlert className="text-red" />}
+        </div>
+        {error && <p className="mt-1 absolute top-9 left-0 text-sm text-red">{error}</p>}
       </div>
     );
   },
