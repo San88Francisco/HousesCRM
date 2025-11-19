8442cb1e3446084fa68cbf5a7868c63a133210ba
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2025-11-19 12:59:42 +0200

    fix: review coderabbitai[bot]

--- client/src/types/core/chart-pie-config.ts
+++ client/src/types/core/chart-pie-config.ts
@@ -1,12 +1,12 @@
 import { THEMES } from '@/shared/constants/pie-chart/chartThemes';
-import React from 'react';
+import { ComponentType, ReactNode } from 'react';
 
 export type ChartPieConfig = {
   [k in string | number]: {
-    label?: React.ReactNode;
-    icon?: React.ComponentType;
+    label?: ReactNode;
+    icon?: ComponentType;
   } & (
-    | { color?: string; theme?: never }
+    | { color: string; theme?: never }
     | { color?: never; theme: Record<keyof typeof THEMES, string> }
   );
 };
