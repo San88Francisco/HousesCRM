ee8ff3e20d578d0bb82b1a6e2f428d1871104829
Vasyl <csgop84@gmail.com>
2025-11-24 19:17:32 +0100

    test

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,10 +1,9 @@
-import BackgroundLogo from '@/widgets/login/background-logo/BackgroundLogo';
 import { LoginForm } from '@/widgets/login/login-form';
 
 export default function Page() {
   return (
     <div className="relative h-screen flex items-center justify-center overflow-hidden bg-background">
-      <BackgroundLogo />
+      {/* <BackgroundLogo /> */}
 
       <div className="relative z-10 w-full flex justify-center">
         <LoginForm />
