5095229dfa6118a92210ce2e2f75b68c55db3ab9
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2025-12-28 12:46:07 +0200

    Folder name changed

--- client/src/components/modals/index.tsx
+++ client/src/components/modals/index.tsx
@@ -1,5 +1,5 @@
 import { ConfirmDeleteModal } from './confirm-delete-modal/ConfirmDeleteModal';
-import { HouseFormModal } from './create-update-house-form';
+import { HouseFormModal } from './house-create-update-modal';
 import { PdfContractModal } from './pdf-contract-modal';
 
 export const ModalRoot = () => {
