0ae07abb551d3a54184740ff875c4a5d6af04d7e
Andrii Dmtryk <admitruk237@gmail.com>
2026-01-07 15:50:59 +0100

    fix: remove extraneous character from scrollbarClasses definition

--- client/src/shared/constants/cn.ts
+++ client/src/shared/constants/cn.ts
@@ -1,6 +1,6 @@
 export const scrollbarClasses = `
   scrollbar-none
-  [&::-webkit-scrollbar]:w-[4px]і
+  [&::-webkit-scrollbar]:w-[4px]
   [&::-webkit-scrollbar]:bg-transparent
   [&::-webkit-scrollbar-thumb]:bg-muted-text
   [&::-webkit-scrollbar-track]:bg-transparent
