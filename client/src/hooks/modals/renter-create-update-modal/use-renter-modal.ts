359c5d6f36753521a3ee39d5d18d809793d30c42
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2026-01-08 14:46:08 +0200

    Refactor code modal state

--- client/src/hooks/modals/renter-create-update-modal/use-renter-modal.ts
+++ client/src/hooks/modals/renter-create-update-modal/use-renter-modal.ts
@@ -1,21 +1,25 @@
-import { isEditModalPayload } from '@/shared/utils/helpers/is-edit-modal-payload';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { closeModal } from '@/store/slice/modal-slice';
+import { ModalState } from '@/types/core/modal-state';
 import { Renter } from '@/types/core/renter';
 import { ModalTriggers } from '@/types/model/modals';
 
+export type RenterModalPayload = {
+  renter?: Renter;
+};
+
 export const useRenterModal = () => {
   const dispatch = useAppDispatch();
-  const { isOpen, trigger, payload } = useAppSelector(s => s.modal);
+  const { isOpen, trigger, payload } = useAppSelector(
+    s => s.modal as ModalState<RenterModalPayload>,
+  );
 
   const isThisModalOpen =
     isOpen && (trigger === ModalTriggers.ADD_RENTER || trigger === ModalTriggers.EDIT_RENTER);
 
   const isEditMode = trigger === ModalTriggers.EDIT_RENTER;
 
-  const renterToEdit = isEditModalPayload<'renter', Renter>(payload, 'renter')
-    ? payload.renter
-    : undefined;
+  const renterToEdit = isEditMode ? payload?.renter : undefined;
 
   const handleClose = (reset: () => void) => {
     dispatch(closeModal());
