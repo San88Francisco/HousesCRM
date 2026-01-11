b3dd20c797e7a35132e0d25b1614958eff821203
Andrii Dmtryk <admitruk237@gmail.com>
2026-01-11 13:00:14 +0100

    feat: create HeaderRenter component and update renter page layout

--- client/app/renter/[id]/page.tsx
+++ client/app/renter/[id]/page.tsx
@@ -1,3 +1,9 @@
+import { HeaderRenter } from '@/widgets/all-houses/renter/HeaderRenter';
+
 export default function Page() {
-  return <section className="flex flex-col md:gap-14 gap-8 mb-10">Renter Page</section>;
+  return (
+    <section className="flex flex-col md:gap-14 gap-8 mb-10">
+      <HeaderRenter />
+    </section>
+  );
 }
---
+++ client/src/widgets/all-houses/renter/HeaderRenter.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { PAGE_LIMIT } from '@/constants/renter';
+import { useGetAllContractsByRenterIdQuery } from '@/store/renters-api';
+import { Building2, DoorOpen, MapPin, PackagePlus, Ruler } from 'lucide-react';
+import { useParams } from 'next/navigation';
+
+export const HeaderRenter = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const { data, isLoading, error } = useGetAllContractsByRenterIdQuery(
+    {
+      renterId: id,
+      page: 1,
+      limit: PAGE_LIMIT,
+      sortBy: 'commencement',
+      order: 'DESC',
+    },
+    {
+      skip: !id,
+    },
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error || !data) return <div>Щось пішло не так</div>;
+
+  const { firstName, lastName, age, occupied, vacated, totalIncome, status } = data.oneRenterReport;
+
+  const items = [
+    { Icon: DoorOpen, text: `${occupied} кімн.` },
+    { Icon: Ruler, text: `${vacated} м²` },
+    { Icon: Building2, text: `${totalIncome} поверх` },
+    { Icon: PackagePlus, text: `Куплено ${status}` },
+  ];
+
+  return (
+    <div className="space-y-2 sm:space-y-3">
+      <h1 className="text-xl sm:text-2xl font-semibold leading-tight">
+        {firstName} {lastName}
+      </h1>
+
+      <div className="flex items-start gap-2 text-sm sm:text-base font-semibold">
+        <MapPin className="w-4 h-4 sm:w-5 sm:h-5 text-muted-text mt-[2px]" />
+        <span className="text-gray-medium break-words">{age}</span>
+      </div>
+
+      <div className="flex flex-wrap gap-2 sm:gap-4 text-xs sm:text-sm text-muted-text">
+        {items.map(({ Icon, text }) => (
+          <div key={text} className="flex items-center gap-1 rounded-full bg-muted/60 px-2 py-1">
+            <Icon className="w-3.5 h-3.5 sm:w-4 sm:h-4 text-muted-text" />
+            <span className="text-gray-medium whitespace-nowrap">{text}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
