aa9b49257dc1fd17da84cc0784f5381759bff980
Andrii Dmtryk <admitruk237@gmail.com>
2026-02-10 14:08:09 +0100

    feat: add `Contract` entity with relationships on the server and a client-side table component for contract management.

--- client/src/widgets/all-contracts/ContractsTable.tsx
+++ client/src/widgets/all-contracts/ContractsTable.tsx
@@ -2,15 +2,13 @@
 
 import { TablePagination } from '@/components/table-pagination';
 import { contractsTableGrid } from '@/shared/constants/styles/contracts-table';
-import { ROUTES } from '@/shared/routes';
+
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/shared/ui/table';
 import { cn } from '@/shared/utils/cn';
 import { formatDate } from '@/shared/utils/format/format-date';
-import { createRowKeyDown } from '@/shared/utils/table/row-key-down-handler';
 import { Contract } from '@/types/core/contract';
 
 import { flexRender, Table as TableType } from '@tanstack/react-table';
-import { useRouter } from 'next/navigation';
 
 type Props = {
   table: TableType<Contract>;
@@ -19,12 +17,6 @@ type Props = {
 };
 
 export const ContractsTable = ({ table, limit, onLimitChange }: Props) => {
-  const { push } = useRouter();
-
-  const handleRouteToContract = (contractId: string) => {
-    push(`${ROUTES.CONTRACT}/${contractId}`);
-  };
-
   return (
     <div className="flex flex-col justify-between h-full">
       <div className="w-full overflow-x-auto">
@@ -50,12 +42,7 @@ export const ContractsTable = ({ table, limit, onLimitChange }: Props) => {
               table.getRowModel().rows.map(row => (
                 <TableRow
                   key={row.original.id}
-                  className={cn(
-                    contractsTableGrid,
-                    'cursor-pointer transition-colors duration-300 ease-out hover:bg-muted-foreground text-text',
-                  )}
-                  onClick={() => handleRouteToContract(row.original.id)}
-                  onKeyDown={createRowKeyDown(() => handleRouteToContract(row.original.id))}
+                  className={contractsTableGrid}
                   tabIndex={0}
                   role="button"
                   aria-label={`Перейти до договору з ${formatDate(row.original.commencement)} по ${formatDate(row.original.termination)}`}
